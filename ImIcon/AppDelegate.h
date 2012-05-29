038f7f3baa5af031a6b73a296db8be9e66af06d5
fairzy <fairzy@gmail.com>
2012-05-29 14:38:01 +0800

    Initial Commit

---
+++ ImIcon/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  ImIcon
+//
+//  Created by fairzy fan on 12-5-29.
+//  Copyright (c) 2012年 PConline. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
